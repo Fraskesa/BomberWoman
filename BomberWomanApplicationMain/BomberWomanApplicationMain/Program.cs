514b0df32427460e91961863978f10bec8af9b2a
Freja <frejasaurbrey@live.dk>
2014-03-07 10:24:21 +0100

    Updated main program
    I have initialised the classes for Bomberwoman (BW), the enemies and the
    bombs.

--- BomberWomanApplicationMain/BomberWomanApplicationMain/Program.cs
+++ BomberWomanApplicationMain/BomberWomanApplicationMain/Program.cs
@@ -6,6 +6,36 @@ using System.Threading.Tasks;
 
 namespace BomberWomanApplicationMain
 {
+    class BomberWoman
+    {
+        float speed; //can be altered by picking up items by multiplying wiht e.g. 1.25
+        bool alive; // in bomberman there is no health bar, the either you are alive or you are dead.
+        uint bombs; //number of bombs available
+        bool bombcontrol; //if BW can control bombs explosions or nor
+        int blast; //the energy of the blast is how long the explosion will reach
+        float xpos; //checking where BW is and makes sure she can't fly over objects.
+        float ypos;
+
+        //need a function to set a bomb
+        //need a function that makes to bomb explode given that BW can control it
+        //a function that checks if the blast hit an enemy, though this might just be opposite - if an enemy was hit by a blast.
+    }
+
+    class Enemy
+    {
+        //needs speed and some AI system I think
+    }
+
+    class Bomb
+    {
+        //The bomb should have a timer or be controlled by BW
+        //a blast length
+        //a spawner
+        //some graphics
+
+        int blast; //the energy of the blast is how long the explosion will reach
+    }
+  
     class Program
     {
         static void Main(string[] args)
