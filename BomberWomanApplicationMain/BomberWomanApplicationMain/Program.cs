a5f983483b1d6552064da977081a3470b49253d9
Martin Kæreby Pedersen <solmar@sol.dk>
2014-04-11 14:02:44 +0200

    Testing something again :)

--- BomberWomanApplicationMain/BomberWomanApplicationMain/Program.cs
+++ BomberWomanApplicationMain/BomberWomanApplicationMain/Program.cs
@@ -9,16 +9,15 @@ namespace BomberWomanApplicationMain
     
     class GameFigure
     {
-        public float xpos; //checking where the figure is, and makes sure it cant fly over objects
-        public float ypos;
-        public bool alive = true;
-        public float speed; //can be altered by picking up items by multiplying with e.g. 1.25
-
+        float xpos; //checking where the figure is, and makes sure it cant fly over objects
+        float ypos;
+        bool alive = true;
+        float speed; //can be altered by picking up items by multiplying with e.g. 1.25
     }
     
     class BomberWoman : GameFigure
     {
-        int lives; //the number of lives left
+		int lives; //the number of lives left
         uint bombs; //number of bombs available
         bool bombcontrol; //if BW can control bombs explosions or nor
         int blast; //the energy of the blast is how long the explosion will reach
@@ -36,18 +35,24 @@ namespace BomberWomanApplicationMain
 
         public void Collision()
         {
-            if (BomberWoman.xpos == Enemy.xpos && BomberWoman.ypos == Enemy.ypos){
-                BomberWoman.lives -= 1;
+			//      if (BomberWoman.xpos == Enemy.xpos && BomberWoman.ypos == Enemy.ypos){
+			//    BomberWoman.lives -= 1;
 
                 //Restart level
-            }    
+			//}    
 
-            if(BomberWoman.lives < 1)
-                BomberWoman.alive = false;
+		//   if(BomberWoman.lives < 1)
+		//       BomberWoman.alive = false;
                 
                 //Game over
-        }
-    }
+		// }
+
+	//		public void Update (){
+		
+		
+		}
+
+	}
 
     class Bomb
     {
@@ -55,14 +60,17 @@ namespace BomberWomanApplicationMain
         //a blast length
         //a spawner
         //some graphics
-
         int blast; //the energy of the blast is how long the explosion will reach
     }
+
+
+
   
     class Program
     {
         static void Main(string[] args)
         {
+
         }
     }
 }
