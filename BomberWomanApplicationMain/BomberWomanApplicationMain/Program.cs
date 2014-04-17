1b90f3d6857bc6cf281e6d3f13bb3c7c4a7692fe
Freja <frejasaurbrey@live.dk>
2014-04-17 09:52:35 +0200

    Added MonoGame
    I have made a new file with monogame using Xna, so that we easier can
    access a graphics library to use. I haven't transfered the classes yet,
    however that will be done later today or tomorrow.

--- BomberWomanApplicationMain/BomberWomanApplicationMain/Program.cs
+++ BomberWomanApplicationMain/BomberWomanApplicationMain/Program.cs
@@ -8,22 +8,26 @@ namespace BomberWomanApplicationMain
 {
     class BomberWoman
     {
-        float speed; //can be altered by picking up items by multiplying wiht e.g. 1.25
-        bool alive; // in bomberman there is no health bar, the either you are alive or you are dead.
-        uint bombs; //number of bombs available
-        bool bombcontrol; //if BW can control bombs explosions or nor
-        int blast; //the energy of the blast is how long the explosion will reach
-        float xpos; //checking where BW is and makes sure she can't fly over objects.
-        float ypos;
+       public float speed; //can be altered by picking up items by multiplying wiht e.g. 1.25
+       public bool alive; // in bomberman there is no health bar, the either you are alive or you are dead.
+       public uint bombs; //number of bombs available
+       public bool bombcontrol; //if BW can control bombs explosions or nor
+       public int lives; //the numbers of lives left (not including the one in use)
+       public int blast; //the energy of the blast is how long the explosion will reach
+       public float xpos; //checking where BW is and makes sure she can't fly over objects.
+       public float ypos;
 
-        //need a function to set a bomb
-        //need a function that makes to bomb explode given that BW can control it
-        //a function that checks if the blast hit an enemy, though this might just be opposite - if an enemy was hit by a blast.
+        //need a method to set a bomb
+        //need a method that makes to bomb explode given that BW can control it
+        //a method0 that checks if the blast hit an enemy, though this might just be opposite - if an enemy was hit by a blast.
     }
 
     class Enemy
     {
         //needs speed and some AI system I think
+        public float xpos;
+        public float ypos;
+        public bool alive;    
     }
 
     class Bomb
@@ -32,8 +36,8 @@ namespace BomberWomanApplicationMain
         //a blast length
         //a spawner
         //some graphics
-
-        int blast; //the energy of the blast is how long the explosion will reach
+        public bool inScene;
+        public int blast; //the energy of the blast is how long the explosion will reach
     }
   
     class Program
