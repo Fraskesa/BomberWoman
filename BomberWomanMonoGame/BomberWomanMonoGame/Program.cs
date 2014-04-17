1afb6a4cb300e6f3ccd66005c70d474d4ede927d
Freja <frejasaurbrey@live.dk>
2014-04-17 12:56:12 +0200

    The New Game Folder
    I have added the new folder with the Mono game verision of the game,
    implementing the classes already written by the group members along with
    some summaries, and additional classes. The classes is made as seperate
    class files, instead of working in the same file.

---
+++ BomberWomanMonoGame/BomberWomanMonoGame/Program.cs
@@ -0,0 +1,23 @@
+﻿#region Using Statements
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+#endregion
+namespace BomberWomanMonoGame
+{
+	static class Program
+	{
+		private static Game1 game;
+
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		[STAThread]
+		static void Main ()
+		{
+			game = new Game1 ();
+			game.Run ();
+		}
+	}
+}
