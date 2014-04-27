9d1ea93e6b5e2e7507de94db25fd95857f95b9e0
Freja <frejasaurbrey@live.dk>
2014-04-27 13:37:28 +0200

    Trying to use teh virtual methods

--- BomberWomanMonoGame/BomberWomanMonoGame/Walls.cs
+++ BomberWomanMonoGame/BomberWomanMonoGame/Walls.cs
@@ -1,16 +1,43 @@
 ﻿using System;
 
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Storage;
+using Microsoft.Xna.Framework.Input;
+
 namespace BomberWomanMonoGame
 {
 	/// <summary>
-	/// Write a summary here
+	/// The Walls are the hedges that can be blown by the blast of the bombs. 
+	/// The hedges sometimes have hidden objects that are only revealed when they have been removed.
 	/// </summary>
 	public class Walls : StaticObject
 	{
 		public Walls ()
 		{
-			//public int pos.x; 
-			//public int pos.y;
+			private Texture2D SpriteHedge;
+
+			public virtual void LoadContent()
+			{
+				SpriteHedge = Content.Load<Texture2D> ("hedge");
+			}
+
+			public virtual void UnloadConent()
+			{
+
+			}
+
+			public virtual void Update (GameTime gameTime)
+			{
+
+			}
+
+			public virtual void Draw (SpriteBatch spritebatch)
+			{
+				spriteBatch.Begin ();
+				spriteBatch.Draw (SpriteHedge, new Rectangle (50, 50, 800, 600), Color.White);
+				spriteBatch.End ();
+			}
 		}
 	}
 }
